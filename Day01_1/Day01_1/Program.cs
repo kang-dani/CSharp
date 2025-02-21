e4634e7de21bf6fdaeb287cdacc3e9f622d6d4f2
kang-dani <daeun4001@gmail.com>
2025-02-21 10:23:13 +0900

    Day01_1

---
+++ Day01_1/Day01_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언
+            int age; //정수형 변수
+            age = 25; //변수에 값 저장
+
+            Console.WriteLine(age); //변수에 저장된 값을 출력
+        }
+    }
+}
